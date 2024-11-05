6adfba6ab3865ddeb0b02c8ce2745f8f6a0bcce1
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-11-05 00:43:16 -0400

    asd
    asda

---
+++ 5-Introduccion a JS/IntroduccionJS/js/04.js
@@ -0,0 +1,6 @@
+const boolean = true,
+  boolean2 = false,
+  boolean3 = "true";
+  console.log('boolean2 :', boolean2);
+  console.log('boolean3 :', boolean3);
+console.log('1' === 1);
---
+++ 5-Introduccion a JS/IntroduccionJS/js/10.js
@@ -0,0 +1,13 @@
+/* Objetos */
+const productoObjeto = {
+  producto: 'Monitor 20 pulgadas',
+  precio: 300,
+  disponible: true,
+};
+let { precio } = productoObjeto;
+console.log('precio :', precio);
+precio = 66;
+console.log('precio :', precio);
+
+
+
