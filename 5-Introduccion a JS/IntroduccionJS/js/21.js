d1bc44bf30a643411d6a3f2675eb9dca13b9b3b4
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-11-23 22:32:48 -0400

    sdfg
    dsfg

---
+++ 5-Introduccion a JS/IntroduccionJS/js/21.js
@@ -0,0 +1,44 @@
+// Array Methods
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+
+const carrito = [
+  { nombre: 'Monitor 20 Pulgadas', precio: 500 },
+  { nombre: 'Televisión 50 Pulgadas', precio: 700 },
+  { nombre: 'Tablet', precio: 300 },
+  { nombre: 'Audifonos', precio: 200 },
+];
+
+// forEach
+meses.forEach((mes) => {
+  if (mes == 'Marzo') {
+    console.log('Marzo si existe');
+  }
+});
+
+// Includes
+let resultado = meses.includes('Marzo');
+console.log(resultado);
+
+// Some ideal para arreglo de objetos
+resultado = carrito.some((producto) => producto.nombre === 'Celular PRO');
+console.log(resultado);
+
+// Reduce
+resultado = carrito.reduce((total, producto) => total + producto.precio, 0);
+console.log(resultado);
+
+// Filter .red
+resultado = carrito.filter((producto) => producto.precio > 400);
+console.log(resultado);
+
+resultado = carrito.filter((producto) => producto.nombre !== 'Audifonos');
+console.log(resultado);
+switch (key) {
+    case value:
+        
+        break;
+cas
+    default:
+        break;
+}
