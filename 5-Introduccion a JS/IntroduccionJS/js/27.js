4bc73197b33ace93d958434630847e03c8922edc
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-12-04 20:50:05 -0400

    ad
    ads

---
+++ 5-Introduccion a JS/IntroduccionJS/js/27.js
@@ -0,0 +1,11 @@
+class Producto {
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
+
+const producto2 = new Producto('Monitor', 800);
+console.log(producto2);
+const producto3 = new Producto('holi', 100);
+console.log(producto3);
