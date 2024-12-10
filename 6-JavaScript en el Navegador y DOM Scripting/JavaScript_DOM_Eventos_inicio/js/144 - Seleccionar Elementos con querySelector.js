66ae733dd086628afa89b92d0e574705f4f2ed4b
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-12-10 19:36:46 -0400

    adsf
    asdf

--- 6-JavaScript en el Navegador y DOM Scripting/JavaScript_DOM_Eventos_inicio/js/144 - Seleccionar Elementos con querySelector.js
+++ 6-JavaScript en el Navegador y DOM Scripting/JavaScript_DOM_Eventos_inicio/js/144 - Seleccionar Elementos con querySelector.js
@@ -1,2 +1,5 @@
-
-console.log('hola')
+//REVIEW - querySelector
+const heading = document.querySelector('.header__texto h2');
+heading.textContent = 'Nuevo heading';
+heading.classList.add('nueva-clase');
+console.log(heading);
