b54e072588cfe87745cdb4f7023e5306d2891ee3
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-12-11 12:02:00 -0400

    ad
    asd

--- 6-JavaScript en el Navegador y DOM Scripting/JavaScript_DOM_Eventos_inicio/js/144 - Seleccionar Elementos con querySelector.js
+++ 6-JavaScript en el Navegador y DOM Scripting/JavaScript_DOM_Eventos_inicio/js/144 - Seleccionar Elementos con querySelector.js
@@ -1,5 +1,37 @@
-//REVIEW - querySelector
+//NOTE - querySelector
+//section - seleccionando un elemento del html
 const heading = document.querySelector('.header__texto h2');
-heading.textContent = 'Nuevo heading';
-heading.classList.add('nueva-clase');
+// #region Explicación
+/*<h2
+  id="heading"
+  class="no-margin"
+>
+  Blog de café con consejos y cursos
+  </h2>*/
+// #endregion
+//!section fin seleccionando un elemento del html
 console.log(heading);
+
+//section - modificando heading
+heading.textContent = 'Nuevo heading';
+// #region heading modificado
+/*<h2
+  id="heading"
+  class="no-margin"
+>
+  Nuevo heading
+</h2>*/
+// #endregion
+//!section fin modificando heading
+
+//section - creando nueva clase
+heading.classList.add('nueva-clase');
+// #region creando nueva clase
+/*<h2
+  id="heading"
+  class="no-margin nueva-clase"
+>
+  Nuevo heading
+</h2>*/
+// #endregion
+//!section fin creando nueva clase
---
+++ 6-JavaScript en el Navegador y DOM Scripting/JavaScript_DOM_Eventos_inicio/js/145 - Seleccionar Elementos con querySelectorAll.js
@@ -0,0 +1,15 @@
+//NOTE - querySelectorAll
+//section - seleccionando todos los elementos ".navegacion a" html
+const enlaces = document.querySelectorAll('.navegacion a');
+/*NodeList(6) [a.navegacion__enlace, a.navegacion__enlace, a.navegacion__enlace, a.navegacion__enlace, a.navegacion__enlace, a.navegacion__enlace]
+0: a.navegacion__enlace //NOTE Nosotros header
+1: a.navegacion__enlace //NOTE Cursos   header
+2: a.navegacion__enlace //NOTE Contacto header
+
+3: a.navegacion__enlace //NOTE Nosotros footer
+4: a.navegacion__enlace //NOTE Cursos   footer
+5: a.navegacion__enlace //NOTE Contacto footer
+length: 6
+[[Prototype]]: NodeList*/
+//!section fin seleccionando todos los elementos ".navegacion a" html
+console.log(enlaces);
