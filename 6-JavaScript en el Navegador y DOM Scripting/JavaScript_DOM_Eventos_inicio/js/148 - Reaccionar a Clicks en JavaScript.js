14d438356d44536ec948e113af87f29c18eca554
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-12-13 23:20:27 -0400

    fqwer
    qwerasdf

--- 6-JavaScript en el Navegador y DOM Scripting/JavaScript_DOM_Eventos_inicio/js/148 - Reaccionar a Clicks en JavaScript.js
+++ 6-JavaScript en el Navegador y DOM Scripting/JavaScript_DOM_Eventos_inicio/js/148 - Reaccionar a Clicks en JavaScript.js
@@ -1,2 +1,14 @@
+// Selecciona el botón con la clase 'boton--primario' del DOM
 const btnEnviar = document.querySelector('.boton--primario');
-console.log(btnEnviar)
+
+// Agrega un event listener al botón para manejar el evento 'click'
+btnEnviar.addEventListener('click', function (event) {
+  // Muestra en la consola el objeto del evento asociado al clic
+  console.log(event);
+
+  // Evita el comportamiento predeterminado del evento (por ejemplo, el envío de un formulario)
+  event.preventDefault();
+
+  // Muestra un mensaje en la consola para confirmar que la acción se realizó
+  console.log('enviando');
+});
