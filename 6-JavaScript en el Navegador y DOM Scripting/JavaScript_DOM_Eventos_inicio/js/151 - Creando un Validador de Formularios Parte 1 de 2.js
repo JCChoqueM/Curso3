038ca4da2b61704ca9c422b3c948379e0bf2c675
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-12-16 01:23:35 -0400

    asd
    asdf

--- 6-JavaScript en el Navegador y DOM Scripting/JavaScript_DOM_Eventos_inicio/js/151 - Creando un Validador de Formularios Parte 1 de 2.js
+++ 6-JavaScript en el Navegador y DOM Scripting/JavaScript_DOM_Eventos_inicio/js/151 - Creando un Validador de Formularios Parte 1 de 2.js
@@ -1,29 +1,47 @@
 const datos = {
-    nombre: '',
-    email: '',
-    mensaje: '',
-  };
-  
-  const nombre = document.querySelector('#nombre');
-  const email = document.querySelector('#email');
-  const mensaje = document.querySelector('#mensaje');
-  
-  nombre.addEventListener('input', leeTexto);
-  
-  email.addEventListener('input', leeTexto);
-  
-  mensaje.addEventListener('input', leeTexto);
-  
-  function leeTexto(event) {
-    datos[event.target.id] = event.target.value;
-  
-  
-    console.log(datos);
-  }
-  
-  const formulario = document.querySelector('.formulario');
+  nombre: '',
+  email: '',
+  mensaje: '',
+};
 
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
+
+nombre.addEventListener('input', leeTexto);
+
+email.addEventListener('input', leeTexto);
+
+mensaje.addEventListener('input', leeTexto);
+
+// El evento de Submit
 formulario.addEventListener('submit', function (event) {
   event.preventDefault();
-  console.log('Enviando Formulario');
+  //validar el formulario
+  const { nombre, email, mensaje } = datos;
+  if (nombre === '' || email === '' || mensaje === '') {
+    mostrarError('Todos los campos son obligatorios');
+    return;
+  }
+  //enviar el formulario
+  console.log('enviando el formulario');
 });
+
+function leeTexto(event) {
+  datos[event.target.id] = event.target.value;
+}
+
+//muestra un error en pantalla
+function mostrarError(mensaje) {
+  const error = document.createElement('P');
+  error.textContent = mensaje;
+  error.classList.add('error');
+
+  formulario.appendChild(error);
+  //desaparesca despúes de x segundos
+  setTimeout(() => {
+    nombre.remove();
+    console.log(error);
+  }, 5000);
+}
