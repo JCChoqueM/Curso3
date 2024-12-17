a1653d1cfc35617a4797ba3409807633bd334f47
Julio Cesar Choque Mamani <jcchoquem23@gmail.com>
2024-12-17 00:09:22 -0400

    dfs
    asdf

--- 6-JavaScript en el Navegador y DOM Scripting/JavaScript_DOM_Eventos_inicio/js/151 - Creando un Validador de Formularios Parte 1 de 2.js
+++ 6-JavaScript en el Navegador y DOM Scripting/JavaScript_DOM_Eventos_inicio/js/151 - Creando un Validador de Formularios Parte 1 de 2.js
@@ -1,37 +1,48 @@
+/* //SECTION -  variables
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
+//!SECTION  fin-variables
+
+//SECTION - objeto datos
 const datos = {
   nombre: '',
   email: '',
   mensaje: '',
 };
+//SECTION  fin-objetos datos
 
-const nombre = document.querySelector('#nombre');
-const email = document.querySelector('#email');
-const mensaje = document.querySelector('#mensaje');
-const formulario = document.querySelector('.formulario');
-
+//SECTION - addEventListener
 nombre.addEventListener('input', leeTexto);
-
 email.addEventListener('input', leeTexto);
-
 mensaje.addEventListener('input', leeTexto);
+//!SECTION  fin-addEventListener
 
-// El evento de Submit
+//SECTION - evento submit
 formulario.addEventListener('submit', function (event) {
   event.preventDefault();
-  //validar el formulario
+  //section - validar formulario
   const { nombre, email, mensaje } = datos;
+  console.log(datos);
   if (nombre === '' || email === '' || mensaje === '') {
     mostrarError('Todos los campos son obligatorios');
-    return;
+    return; //temrina la funcion
   }
-  //enviar el formulario
-  console.log('enviando el formulario');
+  //alerta de que se envio correctamente los datos
+  mostrarCorrecto('Mensaje enviado correctamente');
+  //!section - fin validar formulario
 });
+//!SECTION  fin-evento submit
 
+//SECTION - leer texto
 function leeTexto(event) {
   datos[event.target.id] = event.target.value;
+  console.log(datos);
 }
+//!SECTION  fin-leer texto
 
+//SECTION - mostrar por pantallaError
 //muestra un error en pantalla
 function mostrarError(mensaje) {
   const error = document.createElement('P');
@@ -42,6 +53,22 @@ function mostrarError(mensaje) {
   //desaparesca despúes de x segundos
   setTimeout(() => {
     error.remove();
-    console.log(error);
   }, 5000);
 }
+//!SECTION fin-mostrar por pantallaError
+
+//SECTION - mostrar por pantallaCorrecto
+//muestra una alerta d que se envio correctamente
+function mostrarCorrecto(mensaje) {
+  const alerta = document.createElement('P');
+  alerta.textContent = mensaje;
+  alerta.classList.add('correcto');
+
+  formulario.appendChild(alerta);
+  //desaparesca despúes de x segundos
+  setTimeout(() => {
+    alerta.remove();
+  }, 5000);
+}
+//!SECTION fin-mostrar por pantallaCorrecto
+ */
