b7914fc7443c78f529237025e8ddc9c897e6ed6e
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-06-11 23:16:53 -0400

    usando tipos en las funciones

--- BienesRaices/includes/funciones.php
+++ BienesRaices/includes/funciones.php
@@ -1,8 +1,8 @@
 <?php
 require 'app.php';
-function incluirTemplate($nombre,$inicio=false)
+function incluirTemplate(string $nombre, bool $inicio = false)
 {
-/* echo (TEMPLATES_URL . "/$nombre.php");  */  
+    /* echo (TEMPLATES_URL . "/$nombre.php");  */
     include TEMPLATES_URL . "/$nombre.php";
     // define('TEMPLATES_URL','/templates');   
     // include "includes/temaplates/$nombre.php";
