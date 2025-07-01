31fa89ae909f13e69026eca81c47e44677059e7a
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-06-30 22:25:53 -0400

    Primer commit: estructura inicial y archivos base

---
+++ IntroduccionPHPOOP_Inicio/includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introducción a PHP - Código Con Juan</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+    <div class="contenido">
+        <h1>Desarrollo Web Completo</h1>
+        <p>Introducción a Programación Orientada a Objetos en PHP</p>
+
+
+        <div class="resultado">
