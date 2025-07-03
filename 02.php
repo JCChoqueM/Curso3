cfd5ada81ab15db109c35ceeb6a44f0594a9e856
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-07-03 12:07:00 -0400

    Cambios recientes en el proyecto

--- 02.php
+++ 02.php
@@ -7,21 +7,36 @@ include 'includes/header.php';
 
 class Producto
 {
+    // Public - Se puede acceder y modificar en cualquier lugar (clase y objeto)
+    // protected - Se puede acceder unicamente en la clase
+    // private solo miembros de la misma clase pueden acceder a el
     public function __construct(
         public string $nombre,
         public int $precio,
         public bool $disponible,
 
     ) {}
-    public function mostrarProducto()
+    public function mostrarProducto():void
     {
         echo "El producto es: {$this->nombre} y su precio es: {$this->precio}";
     }
+    public function getNombre(): string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): void
+    {
+        $this->nombre = $nombre;
+    }
 }
+
 $producto = new Producto('tablet', 200, true);
+
 $producto->mostrarProducto();
-
-
+echo "<br>";
+echo $producto->getNombre();
+$producto->setNombre('nuevo producto');
 
 
 echo '<pre>';
