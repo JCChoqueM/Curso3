365a4a178706c41f5357ee71d656d0c0bd8cdeb8
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-07-03 12:05:21 -0400

    Actualiza 03.php: Métodos getter y setter en Producto

--- 03.php
+++ 03.php
@@ -1,4 +1,51 @@
-<?php include 'includes/header.php';
+<?php
+//Abstraccion
+declare(strict_types=1);
+
+include 'includes/header.php';
+
+
+class Producto
+{
+    
+    public function __construct(
+        public string $nombre,
+        public int $precio,
+        public bool $disponible,
+
+    ) {}
+    public function mostrarProducto():void
+    {
+        echo "El producto es: {$this->nombre} y su precio es: {$this->precio}";
+    }
+    public function getNombre(): string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): void
+    {
+        $this->nombre = $nombre;
+    }
+}
+
+$producto = new Producto('tablet', 200, true);
+
+$producto->mostrarProducto();
+echo "<br>";
+echo $producto->getNombre();
+$producto->setNombre('nuevo producto');
+
+
+echo '<pre>';
+var_dump($producto);
+echo '</pre>';
+
+$producto2 = new Producto('TV curva classict', 700, false);
+$producto2->mostrarProducto();
+echo '<pre>';
+var_dump($producto2);
+echo '</pre>';
 
 
 include 'includes/footer.php';
